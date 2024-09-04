727511b205298403edf425307502f54c86c1fcc6
RISHABH MISHRA <rishabhmishra.81e@gmail.com>
2024-09-05 03:29:36 +0530

    feat: Add Footer component
    Added a new Footer component to the project. The Footer component is a simple React functional component that renders a footer element with a text message. This component will be used to display a footer section on each page of the application.
    
    The Footer component is imported and exported in the Footer.jsx file. The component is styled using Tailwind CSS classes to ensure consistent styling across the application.
    
    This commit addresses the need to have a footer section in the application and improves the overall structure and organization of the codebase.

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="w-full  py-4 text-center">
+      <p className="text">Built with love ❤️</p>
+    </footer>
+  );
+};
+
+export default Footer;
